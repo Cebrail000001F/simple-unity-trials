dc157aa1a5806c6b0147c69fea61a2ab41b7cc64
Cebrail Akyüz <103183740+Cebrail000001F@users.noreply.github.com>
2022-04-24 13:47:32 +0300

    prefab-Instantiate Examp

---
+++ Instantiate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    public GameObject Obje2;
+    void Start()
+    {
+     Instantiate(Obje2);
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
